81d9ab9161536a201a89b03ac64f547ef44d3da5
giovanniguilioti <ngiovanni.guilioti@gmail.com>
2023-07-03 19:09:32 -0300

    heap sort

---
+++ algorithm/heapsort.c
@@ -0,0 +1,11 @@
+#include "datastrucutre/heap.h"
+
+int heap_sort(struct heap* heap, int size)
+{
+    for(int i = size; i > 1; --i)
+    {
+        heap->arr[1] ^= heap->arr[i] ^= heap->arr[1] ^= heap->arr[i];
+        --(heap->size);
+        max_heapify(heap, 1);
+    }
+}
